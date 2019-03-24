0b4f0fc4874687b2a996e670035772eb0373cedb
Selma Ahmetovic <sahmetovic1@etf.unsa.ba>
2019-03-24 13:04:56 +0100

    Initial commit 3 services

---
+++ calendar-service/src/test/java/io/team21/calendarservice/CalendarServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.team21.calendarservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ locations-service/src/test/java/io/team21/locationsservice/LocationsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.team21.locationsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocationsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
