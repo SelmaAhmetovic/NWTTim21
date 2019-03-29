10d5f415419afcff5a4c4c393faeb2ad4296652d
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-03-29 12:34:23 +0100

    added models

---
+++ calendar-service/src/main/java/io/team21/calendarservice/model/Calendar.java
@@ -0,0 +1,7 @@
+package io.team21.calendarservice.model;
+
+public class Calendar {
+    private int id;
+    private String name;
+    private int UserId;
+}
---
+++ calendar-service/src/main/java/io/team21/calendarservice/model/Event.java
@@ -0,0 +1,13 @@
+package io.team21.calendarservice.model;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private int id;
+    private String name;
+    private String location;
+    private LocalDateTime time;
+    private String days;
+    private Calendar calendar;
+    private int RoomId;
+}
