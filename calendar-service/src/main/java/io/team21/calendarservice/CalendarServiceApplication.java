992931ece79c39b11ff1765a5bb105c84bb56581
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-04-07 13:42:35 +0200

    pz2 done

--- calendar-service/src/main/java/io/team21/calendarservice/CalendarServiceApplication.java
+++ calendar-service/src/main/java/io/team21/calendarservice/CalendarServiceApplication.java
@@ -1,13 +1,25 @@
 package io.team21.calendarservice;
 
+import io.team21.calendarservice.entities.Calendar;
+import io.team21.calendarservice.repositories.CalendarRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CalendarServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalendarServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarServiceApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner demo(CalendarRepository repository) {
+        return (args) -> {
+            // save a couple of calendars
+            repository.save(new Calendar(1, "First"));
+            repository.save(new Calendar(2));
+        };
+    }
 }
