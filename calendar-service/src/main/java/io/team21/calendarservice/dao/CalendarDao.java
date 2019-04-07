fe7238570b135be2e0e3f35f6e80a751cccfa562
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-04-07 13:12:13 +0200

    removed old files

--- calendar-service/src/main/java/io/team21/calendarservice/dao/CalendarDao.java
+++
@@ -1,9 +0,0 @@
-package io.team21.calendarservice.dao;
-
-import io.team21.calendarservice.entity.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CalendarDao extends JpaRepository<Calendar, Integer> {
-}
--- calendar-service/src/main/java/io/team21/calendarservice/dao/EventDao.java
+++
@@ -1,9 +0,0 @@
-package io.team21.calendarservice.dao;
-
-import io.team21.calendarservice.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventDao extends JpaRepository<Event, Integer> {
-}
