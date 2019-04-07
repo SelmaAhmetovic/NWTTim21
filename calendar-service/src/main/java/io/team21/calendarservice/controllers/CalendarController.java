ffd56d0728bb9c82e2c7d46074ae633ceb8d5c3e
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-04-07 19:04:45 +0200

    controller for calendars done

--- calendar-service/src/main/java/io/team21/calendarservice/controllers/CalendarController.java
+++ calendar-service/src/main/java/io/team21/calendarservice/controllers/CalendarController.java
@@ -1,16 +1,15 @@
 package io.team21.calendarservice.controllers;
 
 import io.team21.calendarservice.entities.Calendar;
+import io.team21.calendarservice.exceptions.RecordNotFoundException;
 import io.team21.calendarservice.repositories.CalendarRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CalendarController {
@@ -25,9 +24,30 @@ public class CalendarController {
         return repository.findAll();
     }
 
+    @GetMapping("/calendar/{id}")
+    public ResponseEntity<Optional> getCalendarById(@PathVariable Integer id) {
+        Optional calendar = repository.findById(id);
+
+        if (!calendar.isPresent()) {
+            throw new RecordNotFoundException("No calendar with id " + id);
+        }
+        return new ResponseEntity<>(calendar, HttpStatus.CREATED);
+    }
+
     @PostMapping
     public ResponseEntity<Calendar> createCalendar(@Valid @RequestBody Calendar calendar) {
         repository.save(calendar);
         return new ResponseEntity<>(calendar, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/calendar/{id}")
+    public ResponseEntity<String> deleteCalendar(@PathVariable Integer id) {
+        Optional optionalCalendar = repository.findById(id);
+
+        if (!optionalCalendar.isPresent()) {
+            throw new RecordNotFoundException("No calendar with id " + id);
+        }
+        repository.deleteById(id);
+        return new ResponseEntity<>("Calendar deleted.", HttpStatus.OK);
     }
 }
