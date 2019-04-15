b511b4b1b29af2273440827f05f9586d8026d2d9
Selma Ahmetovic <sahmetovic1@etf.unsa.ba>
2019-04-15 16:52:40 +0200

    User-Service rabbitmq

--- user-service/src/main/java/io/team21/userservice/UserServiceApplication.java
+++ user-service/src/main/java/io/team21/userservice/UserServiceApplication.java
@@ -8,28 +8,62 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
 
-	@Autowired
-	//private static RoleService roleService;
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(UserServiceApplication.class, args);
-/*
-		if(roleService.countRoles() == 0){
-			RoleModel tmp = new RoleModel();
-			tmp.setName("admin");
-			roleService.addRole(tmp);
-			tmp.setName("student");
-			roleService.addRole(tmp);
-			tmp.setName("professor");
-			roleService.addRole(tmp);
-			tmp.setName("assistant");
-			roleService.addRole(tmp);
-		}*/
+		static final String topicExchangeName = "spring-boot-exchange";
+
+		    static final String queueName = "spring-boot";
+
+		    @Bean
+		    Queue queue() {
+		        return new Queue(queueName, false);
+		    }
+
+		    @Bean
+		    TopicExchange exchange() {
+		        return new TopicExchange(topicExchangeName);
+		    }
+
+		    @Bean
+		    Binding binding(Queue queue, TopicExchange exchange) {
+		        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+		    }
+
+		    @Bean
+		    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+		            MessageListenerAdapter listenerAdapter) {
+		        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		        container.setConnectionFactory(connectionFactory);
+		        container.setQueueNames(queueName);
+		        container.setMessageListener(listenerAdapter);
+		        return container;
+		    }
+
+		    @Bean
+		    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		        return new MessageListenerAdapter(receiver, "receiveMessage");
+		    }
+
+		    public static void main(String[] args) throws InterruptedException {
+		        SpringApplication.run(Application.class, args).close();
+		    }
+
 
 
 	}
