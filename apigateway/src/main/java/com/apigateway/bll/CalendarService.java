83e4185f1e804ec9c3ddd586f07be91bb67ab3d0
Selma Ahmetovic <sahmetovic1@etf.unsa.ba>
2019-04-18 18:59:14 +0200

    Unit testing

--- apigateway/src/main/java/com/apigateway/bll/CalendarService.java
+++ apigateway/src/main/java/com/apigateway/bll/CalendarService.java
@@ -1,11 +1,14 @@
 package com.apigateway.bll;
 import java.sql.Date;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import com.apigateway.dto.Room;
 import com.apigateway.dto.UserModel;
 import com.apigateway.dto.RoomReservation;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CalendarService {
 
@@ -18,5 +21,34 @@ public class CalendarService {
 		Date date = new Date(4,13,2019);
 		reservation.setReservationTime(date);
 		return reservation;		
+	}
+
+	public List<RoomReservation> FilterReservations (List<RoomReservation> reservations, String userId) {
+		int newUserId = Integer.parseInt(userId);
+		List<RoomReservation> filteredReservations = new ArrayList<RoomReservation>();
+		List<RoomReservation> mapped = new ArrayList<RoomReservation>();
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		for(int i=0; i< reservations.size(); i++) {
+			RoomReservation model = objectMapper.convertValue(reservations.get(i), RoomReservation.class);
+			mapped.add(model);
+		}
+		try {
+			for (RoomReservation temp : mapped) {
+				int i=0;
+				int tempId = temp.getUserId();
+				if (tempId == newUserId) {
+					filteredReservations.add(temp);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+		if(filteredReservations.size() !=0) {
+			return filteredReservations;
+		} else {
+			return null;
+		}
 	}
 }
--- apigateway/src/test/java/com/apigateway/ApigatewayApplicationTests.java
+++ apigateway/src/test/java/com/apigateway/ApigatewayApplicationTests.java
@@ -1,5 +1,7 @@
 package com.apigateway;
 
+import com.apigateway.dto.WrapperWithModel;
+import org.h2.engine.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -8,6 +10,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,14 +22,16 @@ import com.apigateway.dto.UserModel;
 
 import com.netflix.discovery.EurekaClient;
 
-import antlr.collections.List;
-
 import com.apigateway.helpers.ApplicationHelper;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ApigatewayApplicationTests {
 
@@ -100,8 +105,57 @@ public class ApigatewayApplicationTests {
 	        assertEquals("first room is not correct",response.get(0).roomName, room1.roomName);
 	        assertEquals("second room is not correct",response.get(1).roomName, room2.roomName);
 	    }
-	 
-	 
-	 
 
+
+
+	@Test
+	public void filterAllReservationsByUser() {
+
+		CalendarService tester = new CalendarService();
+		Room room = new Room();
+		room.setId(2L);
+		room.setLocation("Test Selma");
+		room.setRoomCapacity(35);
+		room.setRoomName("Test Selma");
+
+		RoomReservation roomReservation1 = new RoomReservation();
+		roomReservation1.setReservationTime(null);
+		roomReservation1.setUserId(1);
+		roomReservation1.setRoom(room);
+
+		RoomReservation roomReservation2 = new RoomReservation();
+		roomReservation2.setReservationTime(null);
+		roomReservation2.setUserId(2);
+		roomReservation2.setRoom(room);
+
+		UserModel user = new UserModel();
+		user.setId(1);
+		user.setFirstName("Test name");
+		user.setLastName("Test last name");
+
+		ArrayList<RoomReservation> reservations = new ArrayList<RoomReservation>();
+		reservations.add(roomReservation1);
+		reservations.add(roomReservation2);
+		Mockito
+				.when(helper.getUrl(eurekaClient, "ApiGateway", "/filterReservationsByUser/{userId}"))
+				.thenReturn("http://localhost:8090/filterReservationsByUser/{userId}");
+		Mockito
+				.when(restTemplate.getForObject("http://localhost:8090/api/reservations", ArrayList.class))
+				.thenReturn(reservations);
+
+		List<RoomReservation> reservationFiltered = tester.FilterReservations(reservations, "1");
+
+		boolean idCorrect = true;
+		for (RoomReservation temp : reservationFiltered) {
+			if(temp.getUserId() != user.getId()) {
+				idCorrect = false;
+			}
+		}
+		assertEquals("User ID is not correct",idCorrect, true);
+		if(reservationFiltered.size() !=0)
+			assertEquals("Size is not correct",true, true);
+		else
+			assertEquals("Size is not correct",false, true);
+
+	}
 }
